e9cddb693f20886ce7812efec443390b00b6048d
Geraldine López <geraldinelopez@MacBook-Pro-de-Geraldine-2.local>
2024-08-19 02:17:06 +0200

    Initial commit - cjava - sesion1

--- src/main/java/com/cjava/demo/S01ExpDemo02Application.java
+++ src/main/java/com/cjava/demo/S01ExpDemo02Application.java
@@ -2,12 +2,14 @@ package com.cjava.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class S01ExpDemo02Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(S01ExpDemo02Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(S01ExpDemo02Application.class, args);
+    }
 
 }
--- src/test/java/com/cjava/demo/S01ExpDemo02ApplicationTests.java
+++ src/test/java/com/cjava/demo/S01ExpDemo02ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class S01ExpDemo02ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
